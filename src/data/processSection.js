cb203a9a695bc0247144e4f3f232a7efbca04a6c
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-04-02 13:33:34 -0500

    poniendo imágenes para presentación

--- src/data/processSection.js
+++ src/data/processSection.js
@@ -1,19 +1,19 @@
 export const processOne = [
   {
     id: 1,
-    image: "grow-1-1.png",
+    image: "upgrade1.png",
     title: "Hayward Aqua Rite Chlorinator",
     text: "Aqua Rite Electronic Chlorine Generators (also known as salt chlorinators) are the largest selling system in the world. They automatically produce pure chlorine every day while the pool is being filtered and distribute it evenly throughout the pool via the return jets. Aqua Rite generators use a low concentration of salt dissolved in the pool water (below the level of taste) to produce up to 1.45 lbs of chlorine daily, enough for all residential pools up to 40,000 gallons. They are also NSF certified and approved for use on commercial and public pools. They easily install on standard filtration systems, both for new construction and on existing pools.",
   },
   {
     id: 2,
-    image: "grow-1-2.png",
+    image: "upgrade2.png",
     title: "Jandy Deck Jets",
     text: "Jandy Laminar Jets with LED are an exciting new addition to the Jandy Water Features line. The Jandy Laminar Jets with LED produce a clear arc of water as they gently enter any pool or spa. The water flow can be adjusted to arc up to 7’ high and project out 8’ into the pool. Add the enchantment of fiber optic lighting to your Jandy Laminar Jets to create spectacular nighttime entertainment. Decorative Deck Box Lids are available in three colors to camouflage the units into the backyard landscape. Pebble is the standard lid color, while pewter and black are optional.",
   },
   {
     id: 3,
-    image: "grow-1-3.png",
+    image: "upgrade3.png",
     title: "Hayward ColoLogic Lighting",
     text: "ColorLogic 4.0 lighting incorporates innovation and technology as the leading underwater light. It provides brighter, more vivid colors and light shows that are energy efficient, with feature rich options for pools and spas.",
     lists: [
@@ -26,14 +26,14 @@ export const processOne = [
   },
   {
     id: 4,
-    image: "grow-1-4.png",
+    image: "upgrade4.png",
     title: "Jandy Sheer Descent Waterfalls",
     text: "Sheer Descent Waterfalls Add Beauty And Life to Your Pool — Day and Night! Sheer Descents can produce a wide range of water effects and can easily be adjusted to suit any mood or environment. Sheer Descent Waterfalls are available in lengths from eight inches to eight feet, including curved and extended lip models. Radius And Super Radius Waterfalls: Radius and Super Radius Sheer Descents allow even greater customization for unusual shapes and designs.",
   },
 
   {
     id: 5,
-    image: "grow-1-4.png",
+    image: "upgrade5.png",
     title: "Pentair Colorvision Bubblers",
     text: "The Colorvision Bubbler is a combination of the ColorVision Niche and Pentair GloBrite LED light to create vivid fountain-like water movement. It is the perfect way to showcase and enhance pool features like sun shelves, steps, beach entries and poolscape elements like ponds and fountains. Its low voltage and minimal water flow requirements make them and ideal energy-efficient addition to pools, fountains, ponds and more. It is fully compatible with the IntelliBrite 5G line of LED pool lights, optional IntelliBrite Controller and the Easy Touch or IntelliTouch Automation Systems. Custom program and control light shows using an indoor control panel, wireless remote and the Screen Logic Interface using your smartphone, computer or mobile digital device.",
   },
@@ -42,7 +42,7 @@ export const processOne = [
 export const processTwo = [
   {
     id: 1,
-    image: "grow-1-1.png",
+    image: "slides1.png",
     title: "",
     text: "This slide will make a big splash with the little one's that enjoy your pool! The Cyclone incorporates many of the same great features of our TurboTwister, but in smaller proportions at just 4 feet tall. Loaded with safety features like molded handrails and an enclosed ladder, this slide fully complies with the US Consumer Product Safety Commission's Safety Standard for Swimming Pool Slides (16 CFR 1207)",
     lists: [
@@ -54,7 +54,7 @@ export const processTwo = [
   },
   {
     id: 2,
-    image: "grow-1-1.png",
+    image: "slides2.png",
     title: "",
     text: "The TurboTwister gives you waterpark thrills in your own backyard! Whether it's the rushing water or thrilling dips and curves that you love, the TurboTwister is packed with tons of great features for the entire family. With sturdy molded-in handrails and an enclosed ladder, we've made safety a top priority.",
     lists: [
